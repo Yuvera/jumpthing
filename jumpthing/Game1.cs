420971e6b458b12fe82965f9a0148c2b154f881b
Yuvera <73469427+Yuvera@users.noreply.github.com>
2020-11-10 14:09:14 +0000

    Initial commit

---
+++ jumpthing/Game1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace jumpthing
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D backgroundTxr, playerSheetTxr, platformSheetTxr, collisionTxr;
+
+        Point screenSize = new Point(800, 450);
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = screenSize.X;
+            _graphics.PreferredBackBufferHeight = screenSize.Y;
+            _graphics.ApplyChanges();
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            backgroundTxr = Content.Load<Texture2D>("JumpThing_background");
+            playerSheetTxr = Content.Load<Texture2D>("JumpThing_spriteSheet1");
+            platformSheetTxr = Content.Load<Texture2D>("JumpThing_spriteSheet2");
+
+            collisionTxr = new Texture2D(GraphicsDevice, 1, 1);
+            collisionTxr.SetData(new[] Color.Red, );
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(backgroundTxr, new Rectangle(0, 0, screenSize.X, screenSize.Y), Color.White);
+
+            _spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ jumpthing/Sprite.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jumpthing
+{
+    class Sprite
+    {
+
+        public Texture2D spriteSheetTxr, collisionTxr;
+        public Vector2 spriteLocation, spriteVelocity, spriteOffset, collisionScale, collisionOffset;
+        public bool isDead, isColliding, drawCollision, isFlipped;
+
+        public int currentAnim, currentFrame;
+        public float frameTime, frameCounter;
+        public List<List<Rectangle>> animations;
+
+        public Sprite(Texture2D newSpriteSheet, Texture2D newCollisionTxr, Vector2 newLocation)
+        {
+            spriteSheetTxr = newSpriteSheet;
+            collisionTxr = newCollisionTxr;
+            spriteLocation = newLocation;
+
+            spriteOffset = new Vector2(0.5f, 0.5f);
+            spriteVelocity = new Vector2(0f, 0f);
+            collisionScale = new Vector2(1f, 1f);
+            collisionOffset = new Vector2(0f, 0f);
+            isColliding = false;
+            drawCollision = false;
+            isDead = false;
+            currentAnim = 0;
+            currentFrame = 0;
+            frameTime = 0.5f;
+            frameCounter = frameTime;
+
+            animations = new List<List<Rectangle>>();
+            animations.Add(new List<Rectangle>());
+            animations[0].Add(new Rectangle(0, 0, 48, 48));
+        }
+
+        public virtual void Update(GameTime gameTime, Point screenSize) { }
+    }
+}
